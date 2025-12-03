ed644b895f0ebe7b803df0587d066a386c3e19a5
Willkie <8230127@estg.ipp.pt>
2025-12-03 16:53:03 +0000

    failed setUp

---
+++ SI-Fitza/src/main/java/com/camunda/academy/App.java
@@ -0,0 +1,10 @@
+package com.camunda.academy;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SI-Fitza/src/main/java/com/camunda/academy/PaymentApplication.java
@@ -0,0 +1,20 @@
+package com.camunda.academy;
+
+public class PaymentApplication {
+    private static String CAMUNDA_AUTHORIZATION_SERVER_URL = "https://login.cloud.camunda.io/oauth/token";
+    private static String CAMUNDA_TOKEN_AUDIENCE = "zeebe.camunda.io";
+    private static String CAMUNDA_REST_ADDRESS = "https://bru-2.zeebe.camunda.io/dfdb8d36-5bf6-4b20-be42-8205ce0805f0";
+    private static String CAMUNDA_GRPC_ADDRESS = "https://dfdb8d36-5bf6-4b20-be42-8205ce0805f0.bru-2.zeebe.camunda.io:443";
+    private static String CAMUNDA_CLIENT_ID = "zbk3brnZhoUZZAcTKONeV2o_55vZmy2s";
+    private static String CAMUNDA_CLIENT_SECRET = "aWg6XnQV0r7VsUGJP5cu7IMjaV0WFx5mCvbaWrS1z5dVNJfLWeXDST5aTVb93qea";
+
+    public static void main(String[] args) {
+        final OAuthCredentialsProvider credentialsProvider = new OAuthCredentialsProviderBuilder()
+                .authorizationServerUrl(CAMUNDA_AUTHORIZATION_SERVER_URL)
+                .audience(CAMUNDA_TOKEN_AUDIENCE)
+                .clientId(CAMUNDA_CLIENT_ID)
+                .clientSecret(CAMUNDA_CLIENT_SECRET)
+                .build();
+
+    }
+}
