60c54a9154cedbe59895ee873d6d5d2172fa8948
Miguel <8220319@estg.ipp.pt>
2025-12-08 18:26:43 +0000

    Update

--- SI-Fitza/src/main/java/com/camunda/handles/ProgramarProducao/CalculateRawMaterialNeedsHandle.java
+++ SI-Fitza/src/main/java/com/camunda/handles/ProgramarProducao/CalculateRawMaterialNeedsHandle.java
@@ -43,6 +43,7 @@ public class CalculateRawMaterialNeedsHandle implements JobHandler {
                 System.out.println("   > A processar item: " + type + " (Qtd: " + quantidadePizzas + ")");
 
                 // Obter a ficha técnica (Simulação)
+                // AQUI É ONDE O ERRO OCORRIA: O método getMockTechnicalSheet agora existe lá em baixo
                 ProductTechnicalSheet sheet = getMockTechnicalSheet(type);
 
                 // Calcular totais para este item
@@ -51,7 +52,6 @@ public class CalculateRawMaterialNeedsHandle implements JobHandler {
                     double totalQty = mat.getQuantity() * quantidadePizzas;
 
                     // Adicionar à lista final
-                    // Nota: Numa app real, devias agrupar materiais iguais (ex: somar Farinha de todas as pizzas)
                     RawMaterial rm = mat.getRawMaterial();
                     totalMaterialsNeeded.add(new MaterialNeeded(rm, (int) Math.ceil(totalQty)));
                 }
@@ -75,6 +75,54 @@ public class CalculateRawMaterialNeedsHandle implements JobHandler {
                     .errorMessage("Erro ao calcular materiais: " + e.getMessage())
                     .send()
                     .join();
+        }
+    }
+
+
+    private ProductTechnicalSheet getMockTechnicalSheet(TypePizza type) {
+        // Ingredientes Base
+        RawMaterial flour = new RawMaterial("RM-001", "Farinha Tipo 65", null);
+        RawMaterial yeast = new RawMaterial("RM-002", "Fermento de Padeiro", null);
+        RawMaterial cheese = new RawMaterial("RM-003", "Queijo Mozzarella", null);
+        RawMaterial pepperoni = new RawMaterial("RM-004", "Pepperoni Fatiado", null);
+        RawMaterial ham = new RawMaterial("RM-005", "Fiambre", null);
+        RawMaterial veggies = new RawMaterial("RM-006", "Vegetais Variados", null);
+        RawMaterial tomatoSauce = new RawMaterial("RM-007", "Molho de Tomate", null);
+
+        // Definição das Receitas
+        switch (type) {
+            case PEPPERONI:
+                return new ProductTechnicalSheet(type, "Pizza Pepperoni", new MaterialNeeded[]{
+                        new MaterialNeeded(flour, 1),
+                        new MaterialNeeded(tomatoSauce, 1),
+                        new MaterialNeeded(cheese, 1),
+                        new MaterialNeeded(pepperoni, 2)
+                });
+            case FOUR_CHESSES:
+                return new ProductTechnicalSheet(type, "Pizza 4 Queijos", new MaterialNeeded[]{
+                        new MaterialNeeded(flour, 1),
+                        new MaterialNeeded(tomatoSauce, 1),
+                        new MaterialNeeded(cheese, 3) // Dose tripla de queijo
+                });
+            case VEGETARIAN:
+                return new ProductTechnicalSheet(type, "Pizza Vegetariana", new MaterialNeeded[]{
+                        new MaterialNeeded(flour, 1),
+                        new MaterialNeeded(tomatoSauce, 1),
+                        new MaterialNeeded(veggies, 2)
+                });
+            case CHEESE_COLD_CUTS:
+                return new ProductTechnicalSheet(type, "Pizza Fiambre", new MaterialNeeded[]{
+                        new MaterialNeeded(flour, 1),
+                        new MaterialNeeded(tomatoSauce, 1),
+                        new MaterialNeeded(cheese, 1),
+                        new MaterialNeeded(ham, 2)
+                });
+            default:
+                // Base Genérica
+                return new ProductTechnicalSheet(type, "Massa Base", new MaterialNeeded[]{
+                        new MaterialNeeded(flour, 1),
+                        new MaterialNeeded(yeast, 1)
+                });
         }
     }
 }
